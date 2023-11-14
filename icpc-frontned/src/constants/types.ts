c4ca53127eb0dc2df369f1b7738ee8c0b8653b5a
iYair CC <al046638@uacam.mx>
2023-11-14 11:53:57 -0600

    feature: added enums for text sections, added prettier configuration and corrected the spelling of the base colors.

---
+++ icpc-frontned/src/constants/types.ts
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+
+export enum enumTextSizes {
+  s36 = '36',
+  s22 = '22',
+  s21 = '21',
+  s20 = '20',
+  s18 = '18',
+  s17 = '17',
+  s16 = '16',
+  s14 = '14',
+  s13 = '13',
+  s12 = '12',
+  s10 = '10',
+  s11 = '11',
+  s9 = '9',
+  s8 = '8',
+  s7 = '7'
+}
+
+export enum enumTextTags {
+  h1 = 'h1',
+  h2 = 'h2',
+  h3 = 'h3',
+  h4 = 'h4',
+  h5 = 'h5',
+  h6 = 'h6',
+  p = 'p',
+  span = 'span',
+  label = 'label',
+  a = 'a',
+  ul = 'ul',
+
+  li = 'li',
+  div = 'div'
+}
+
+type typeReactNode = ReactNode
