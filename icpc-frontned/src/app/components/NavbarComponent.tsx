3a2e28e5d56a60e1d42ad7aa724f48555cdbc66f
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2023-11-15 13:35:23 -0600

    fix DropdownComponent Icon now changes dinamically Resolve The icon is selected according to the "open" property of the Menu tag

--- icpc-frontned/src/app/components/NavbarComponent.tsx
+++ icpc-frontned/src/app/components/NavbarComponent.tsx
@@ -46,93 +46,104 @@ const routes = [
 
 export default function NavbarComponent() {
   return (
-    <Disclosure as="nav" className="bg-white shadow">
+    <Disclosure
+      as='nav'
+      className='bg-white shadow'>
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
-            <div className="flex h-16 justify-between">
-              <div className="flex px-2 lg:px-0">
-                <div className="flex flex-shrink-0 items-center">
+          <div className='mx-auto max-w-7xl px-2 sm:px-4 lg:px-8'>
+            <div className='flex h-16 justify-between'>
+              <div className='flex px-2 lg:px-0'>
+                <div className='flex flex-shrink-0 items-center'>
                   <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
+                    className='h-8 w-auto'
+                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                    alt='Your Company'
                   />
                 </div>
-                <div className="hidden items-center lg:ml-6 lg:flex lg:space-x-8">
+                <div className='hidden items-center lg:ml-6 lg:flex lg:space-x-8'>
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                  {routes.map((route) => (
-                    <TextComponent key={route.id}
-                    tag={enumTextTags.a}
-                    sizeFont={enumTextSizes.s12}
-                    href={route.href}
-                    className="flex items-center border-b-2 border-transparent px-1 pt-1 text-black hover:border-gray-300 hover:text-gray-700"
-                    >{route.name}</TextComponent>
+                  {routes.map(route => (
+                    <TextComponent
+                      key={route.id}
+                      tag={enumTextTags.a}
+                      sizeFont={enumTextSizes.s12}
+                      href={route.href}
+                      className='flex items-center border-b-2 border-transparent px-1 pt-1 text-black hover:border-gray-300 hover:text-gray-700'>
+                      {route.name}
+                    </TextComponent>
                   ))}
                 </div>
               </div>
-              <div className="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end">
-                <div className="w-full max-w-lg lg:max-w-xs">
-                  <label htmlFor="search" className="sr-only">
+              <div className='flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end'>
+                <div className='w-full max-w-lg lg:max-w-xs'>
+                  <label
+                    htmlFor='search'
+                    className='sr-only'>
                     Search
                   </label>
-                  <div className="relative">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                      <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  <div className='relative'>
+                    <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
+                      <MagnifyingGlassIcon
+                        className='h-5 w-5 text-gray-400'
+                        aria-hidden='true'
+                      />
                     </div>
                     <input
-                      id="search"
-                      name="search"
-                      className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      placeholder="Search"
-                      type="search"
+                      id='search'
+                      name='search'
+                      className='block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                      placeholder='Search'
+                      type='search'
                     />
                   </div>
                 </div>
               </div>
-              <div className="flex items-center lg:hidden">
+              <div className='flex items-center lg:hidden'>
                 {/* Mobile menu button */}
                 <DropdownComponent options={routes} />
               </div>
-              <div className="hidden lg:ml-4 lg:flex lg:items-center">
+              <div className='hidden lg:ml-4 lg:flex lg:items-center'>
                 <button
-                  type="button"
-                  className="relative flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  type='button'
+                  className='relative flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+                  <span className='absolute -inset-1.5' />
+                  <span className='sr-only'>View notifications</span>
+                  <BellIcon
+                    className='h-6 w-6'
+                    aria-hidden='true'
+                  />
                 </button>
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-4 flex-shrink-0">
+                <Menu
+                  as='div'
+                  className='relative ml-4 flex-shrink-0'>
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
+                    <Menu.Button className='relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+                      <span className='absolute -inset-1.5' />
+                      <span className='sr-only'>Open user menu</span>
                       <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        className='h-8 w-8 rounded-full'
+                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                        alt=''
                       />
                     </Menu.Button>
                   </div>
                   <Transition
                     as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    enter='transition ease-out duration-100'
+                    enterFrom='transform opacity-0 scale-95'
+                    enterTo='transform opacity-100 scale-100'
+                    leave='transition ease-in duration-75'
+                    leaveFrom='transform opacity-100 scale-100'
+                    leaveTo='transform opacity-0 scale-95'>
+                    <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
+                            href='#'
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
                             Your Profile
                           </a>
                         )}
@@ -140,9 +151,8 @@ export default function NavbarComponent() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
+                            href='#'
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
                             Settings
                           </a>
                         )}
@@ -150,9 +160,8 @@ export default function NavbarComponent() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
+                            href='#'
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
                             Sign out
                           </a>
                         )}
@@ -163,8 +172,6 @@ export default function NavbarComponent() {
               </div>
             </div>
           </div>
-
-          
         </>
       )}
     </Disclosure>
