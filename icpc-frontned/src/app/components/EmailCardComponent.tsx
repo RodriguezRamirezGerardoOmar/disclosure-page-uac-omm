820040cd423ba88d86a2469094e166d511c960d1
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2023-11-28 20:30:25 -0600

    Create (EmailCardComponent) New input form created The EmailCardComponent is the first step of the password recovery process The EmailCardComponent uses the React Hook Forms library A new route was created: /forgotPassword

---
+++ icpc-frontned/src/app/components/EmailCardComponent.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { SubmitHandler, useForm } from 'react-hook-form'
+import LogoComponent from './LogoComponent'
+import { TextComponent } from './TextComponent'
+import { enumTextSizes, enumTextTags } from '@/constants/types'
+import Link from 'next/link'
+
+interface IInputCardProps {
+  label: string
+}
+
+export default function LoginCardComponent({ label }: Readonly<IInputCardProps>) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data)
+
+  const textFieldClassname =
+    'block w-full rounded-md border-0 m-2 p-2 text-dark-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-accent sm:leading-6'
+  const labelClassname = 'place-self-start dark:text-dark-accent m-1'
+
+  return (
+    <div className='md:mx-auto max-w-7xl md:px-4 w-full h-full lg:px-8 lg:w-2/3 lg:h-auto'>
+      {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+      <div className='mx-auto max-w-3xl dark:bg-dark-primary rounded-md p-2'>
+        <div className='w-full grid grid-cols-1 place-items-center text-li'>
+          <LogoComponent size={150} />
+          <TextComponent
+            tag={enumTextTags.h3}
+            sizeFont={enumTextSizes.s36}
+            className='dark:text-dark-accent'>
+            {label}
+          </TextComponent>
+        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='m-2 flex flex-col columns-1 place-items-center'>
+          {/* register your input into the hook by invoking the "register" function */}
+          {/* include validation with required or other standard HTML validation rules */}
+          <label
+            htmlFor='exampleRequired'
+            className={labelClassname}>
+            Ingresa tu correo electrónico
+          </label>
+          <input
+            {...register('exampleRequired', { required: true })}
+            className={textFieldClassname}
+            type='email'
+          />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <input
+            type='submit'
+            className='max-w-min bg-primary rounded-md py-2 px-4 text-complementary dark:bg-dark-accent m-2'
+          />
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ icpc-frontned/src/app/forgotPassword/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between pt-24'>
+      <EmailCardComponent label='Recuperar contraseña'/>
+    </main>
+  )
+}
