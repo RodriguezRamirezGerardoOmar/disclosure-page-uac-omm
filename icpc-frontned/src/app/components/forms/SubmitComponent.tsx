a1e421f308b87ffa612cbb208958b9c144482b3a
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2023-12-07 11:12:55 -0600

    Fix (SubmitComponent) added text prop The new text prop is an optional paramter that allows the control of the text in the SubmitComponent

--- icpc-frontned/src/app/components/forms/SubmitComponent.tsx
+++ icpc-frontned/src/app/components/forms/SubmitComponent.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const SubmitComponent = () => {
+interface ISubmitProps {
+  text?: string
+}
+
+const SubmitComponent = ({text}: Readonly<ISubmitProps>) => {
   return (
     <input
       type='submit'
       className='max-w-min bg-primary rounded-md py-2 px-4 text-complementary dark:bg-dark-accent m-2'
+      value={text}
     />
   )
 }
