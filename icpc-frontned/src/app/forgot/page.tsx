36609e6ddf360c1d0cb27fb52dd72297b9b3da6b
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2023-12-18 12:50:56 -0600

    Remove (EmailCardComponent) File deleted The EmailCardComponent.tsx file has been deleted The Recovery email form has been rebuilt using BasicPanelComponent Functionality has been tested and is working Responsive design has been tested and is working

--- icpc-frontned/src/app/forgot/page.tsx
+++ icpc-frontned/src/app/forgot/page.tsx
@@ -1,28 +1,44 @@
 'use client'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import EmailCardComponent from '../components/cards/EmailCardComponent'
+import { SubmitHandler, useForm, FieldValues } from 'react-hook-form'
 import TextFieldComponent from '../components/forms/TextFieldComponent'
+import { BasicPanelComponent } from '../components/panels/BasicPanelComponent'
+import SubmitComponent from '../components/forms/SubmitComponent'
+import LogoComponent from '../components/LogoComponent'
+import { enumTextTags } from '@/constants/types'
+import { TextComponent } from '../components/text/TextComponent'
 
-const onSubmit: SubmitHandler<FormData> = () => {}
+const onSubmit: SubmitHandler<FieldValues> = () => {}
 
 export default function Home() {
-  const methods = useForm()
+  const methods = useForm<FieldValues>()
 
   return (
     <main className='grid min-h-screen grid-cols-1 place-items-center justify-between py-24'>
-      <EmailCardComponent
-        label='Recuperar contraseña'
-        onSubmit={onSubmit}>
-        <TextFieldComponent
-          labelText='Ingresa tu correo electrónico'
-          register={methods.register}
-          fieldName='email'
-          auto='email'
-          type='email'
-          necessary={true}
-          id='email'
-        />
-      </EmailCardComponent>
+      <BasicPanelComponent backgroundColor='bg-gray-300 dark:bg-dark-primary'>
+        <div className='grid grid-cols-1 place-items-center justify-between'>
+          <LogoComponent size={150} />
+          <TextComponent
+            tag={enumTextTags.h3}
+            sizeFont='s36'
+            className='dark:text-dark-accent'>
+            Recuperación de contraseña
+          </TextComponent>
+        </div>
+        <form
+          onSubmit={methods.handleSubmit(onSubmit)}
+          className='grid grid-cols-1 place-items-center justify-between'>
+          <TextFieldComponent
+            labelText='Ingresa tu correo electrónico'
+            register={methods.register}
+            fieldName='email'
+            auto='email'
+            type='email'
+            necessary={true}
+            id='email'
+          />
+          <SubmitComponent text='Enviar correo' />
+        </form>
+      </BasicPanelComponent>
     </main>
   )
 }
