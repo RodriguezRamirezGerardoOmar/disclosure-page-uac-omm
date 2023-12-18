1d79f753e3ba1b51c396e9d25b987b7ef02fb4d2
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2023-12-18 13:19:58 -0600

    Remove (LoginCardComponent) File deleted The LoginCardComponent.tsx file has been deleted The Login form has been remade using the BasicPanelComponent Functionality has been testd and is working Responsive design has been tested and is working

--- icpc-frontned/src/app/login/page.tsx
+++ icpc-frontned/src/app/login/page.tsx
@@ -1,42 +1,58 @@
 'use client'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import LoginCardComponent from '../components/cards/LoginCardComponent'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import CheckboxComponent from '../components/forms/CheckboxComponent'
 import TextFieldComponent from '../components/forms/TextFieldComponent'
+import { BasicPanelComponent } from '../components/panels/BasicPanelComponent'
+import LogoComponent from '../components/LogoComponent'
+import { enumTextTags } from '@/constants/types'
+import SubmitComponent from '../components/forms/SubmitComponent'
+import { TextComponent } from '../components/text/TextComponent'
 
-const onSubmit: SubmitHandler<FormData> = () => {}
+const onSubmit: SubmitHandler<FieldValues> = () => {}
 
 export default function Home() {
-  const methods = useForm()
+  const methods = useForm<FieldValues>()
   return (
     <main className='grid min-h-screen grid-cols-1 place-items-center justify-between py-24'>
-      <LoginCardComponent
-        onSubmit={onSubmit}
-        label='Iniciar sesión'>
-        <TextFieldComponent
-          labelText='Correo electrónico o nombre de usuario'
-          fieldName='username'
-          id='username'
-          necessary={true}
-          type='username'
-          auto='username'
-          register={methods.register}
-        />
-        <TextFieldComponent
-          labelText='Contraseña'
-          fieldName='password'
-          id='password'
-          necessary={true}
-          type='password'
-          auto='current-password'
-          register={methods.register}
-        />
-        <CheckboxComponent
-          labelText='Recuérdame'
-          fieldName='rememberMe'
-          register={methods.register}
-        />
-      </LoginCardComponent>
+      <BasicPanelComponent backgroundColor='bg-gray-300 dark:bg-dark-primary'>
+        <div className='grid grid-cols-1 place-items-center justify-between'>
+          <LogoComponent size={150} />
+          <TextComponent
+            tag={enumTextTags.h3}
+            sizeFont='s36'
+            className='dark:text-dark-accent'>
+            Inicio de sesión
+          </TextComponent>
+        </div>
+        <form
+          onSubmit={methods.handleSubmit(onSubmit)}
+          className='grid grid-cols-1 place-items-center justify-between'>
+          <TextFieldComponent
+            labelText='Correo electrónico o nombre de usuario'
+            register={methods.register}
+            fieldName='username'
+            auto='username'
+            type='username'
+            necessary={true}
+            id='username'
+          />
+          <TextFieldComponent
+            labelText='Correo electrónico o nombre de usuario'
+            register={methods.register}
+            fieldName='password'
+            auto='current-password'
+            type='password'
+            necessary={true}
+            id='password'
+          />
+          <CheckboxComponent
+            labelText='Recuérdame'
+            register={methods.register}
+            fieldName='rememberMe'
+          />
+          <SubmitComponent text='Iniciar sesión' />
+        </form>
+      </BasicPanelComponent>
     </main>
   )
 }
