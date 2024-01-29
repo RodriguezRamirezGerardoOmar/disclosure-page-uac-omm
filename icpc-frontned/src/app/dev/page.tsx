57ff19e5eb8fc2ba9c61ffeb2c0f681bc2ae255e
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2024-01-29 14:30:20 -0600

    fix(footer component): Create a footer component
    WIP, created a component with a title

--- icpc-frontned/src/app/dev/page.tsx
+++ icpc-frontned/src/app/dev/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 import React from 'react'
+import { TextComponent } from '../components/text/TextComponent'
+import { enumTextTags } from '@/constants/types'
 
 export default function Home() {
   return (
-    <div className="border-dashed border-2 border-dark-complementary"></div>
+    <div className='border-dashed border-2 border-dark-complementary'>
+      <TextComponent tag={enumTextTags.h1}>Making the world a better place through construction alegant hieararchies</TextComponent>
+    </div>
   )
 }
