29611f7030705700cf5b9a3cffc2d2a5414df5da
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2025-08-14 22:13:37 -0600

    feat(cloudStorage): Enabled image storage with Google Cloud Storage
    Switched the image storage from the untested Drive API to Google Cloud Storage

--- icpc-backend/src/image/image.service.ts
+++ icpc-backend/src/image/image.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateImageDto } from './dto/update-image.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -6,26 +6,26 @@ import { Image } from './entities/image.entity';
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 import { createHash } from 'crypto';
-import google, { Auth, drive_v3 } from 'googleapis';
+import { Storage } from '@google-cloud/storage';
 
 /*
 Input:
   - create: file (Express.Multer.File)
   - findAll: none
   - findOne: id (string)
-  - update: id (string), updateImageDto (fields to update)
+  - update: id (string), updateImageDto (Express.Multer.File)
   - remove: id (string)
 Output:
   - create: Created or existing image
   - findAll: List of images
-  - findOne: File path of the image
+  - findOne: Image file from the cloud storage
   - update: Updated image
   - remove: Deleted image
 Return value: Service for CRUD operations on images, with file storage and database persistence
 Function: Handles business logic for uploading, retrieving, updating, and deleting images, storing files and metadata
 Variables: imageRepository
-Date: 02 - 06 - 2025
-Author: Alan Julian Itzamna Mier Cupul
+Date: 14 - 08 - 2025
+Author: Gerardo Omar Rodriguez Ramirez
 */
 
 @Injectable()
@@ -35,16 +35,22 @@ export class ImageService {
     private readonly imageRepository: Repository<Image>
   ) {}
 
-  async authorize() {
-    const keyData = fs.readFileSync(process.env.GOOGLE_DRIVE_CREDENTIALS);
-    const key = JSON.parse(keyData.toString());
-    const jwtClient = new Auth.JWT({
-      email: key.client_email,
-      key: key.private_key,
-      scopes: ['https://www.googleapis.com/auth/drive']
-    });
-    await jwtClient.authorize();
-    return jwtClient;
+  /*
+  Input: None
+  Output: An instance of Storage
+  Return value: Storage object
+  Function: Creates an instance of Storage to interact with the Google Cloud Storage service
+  Variables: projectId, keyFilename, bucketName
+  Date: 14 - 08 - 2025
+  Author: Gerardo Omar Rodriguez Ramirez
+  */
+
+  async getBucket() {
+    const bucket = new Storage({
+      projectId: 'intrepid-abacus-360419',
+      keyFilename: process.env.GOOGLE_DRIVE_CREDENTIALS
+    }).bucket(process.env.ASSETS_PATH);
+    return bucket;
   }
 
   async create(file: Express.Multer.File) {
@@ -73,39 +79,22 @@ export class ImageService {
           }
         }
       );*/
-      const auth = await this.authorize();
-
-      const drive = new drive_v3.Drive({
-        auth: auth
-      });
-
-      const res = drive.files.create(
-        {
-          auth: auth,
-
-          requestBody: {
-            name: image.assetName,
-
-            parents: [process.env.ASSETS_PATH],
-
-            mimeType: file.mimetype
-          },
-
-          media: {
-            mimeType: file.mimetype,
-
-            body: file.buffer
-          },
-
-          fields: 'id'
-        },
-
-        function (err) {
-          if (err) {
-            throw err;
-          }
-        }
+      const bucket = await this.getBucket();
+      const res = await bucket.upload(
+        `${process.cwd()}/publicAssets/${image.assetName}`,
+        { destination: image.assetName }
       );
+      if (res[0].metadata) {
+        fs.rm(
+          `${process.cwd()}/publicAssets/${image.assetName}`,
+          { recursive: true },
+          err => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      }
       return await this.imageRepository.save(image);
     } else {
       // If the image already exists, return the existing image metadata
@@ -119,88 +108,42 @@ export class ImageService {
 
   async findOne(id: string) {
     const image = await this.imageRepository.findOneBy({ id: id });
-    const auth = await this.authorize();
-    const drive = new drive_v3.Drive({
-      auth: auth
-    });
-
-    const res = await drive.files.list({
-      q: `'${process.env.ASSETS_PATH}' in parents and name='${image.assetName}'`,
-
-      fields: 'files(id, name)'
-    });
-
-    const file = await drive.files.get(
-      {
-        fileId: res.data.files[0].id,
-
-        alt: 'media'
-      },
-
-      {
-        responseType: 'stream'
-      }
-    );
-
-    return { mimeType: image.mimeType, name: image.assetName, data: file.data };
+    if (image == null) {
+      throw new NotFoundException('Imagen no encontrada.');
+    }
+    const bucket = await this.getBucket();
+    const file = await bucket.file(image.assetName).get();
+    return {
+      mimeType: image.mimeType,
+      name: image.assetName,
+      data: file[0].createReadStream()
+    };
   }
 
-  async update(id: string, updateImageDto: UpdateImageDto) {
+  async update(id: string, updateImageDto: Express.Multer.File) {
     const image = await this.imageRepository.findOneBy({ id: id });
-    const auth = await this.authorize();
-
-    const drive = new drive_v3.Drive({
-      auth: auth
-    });
-
-    const res = await drive.files.list({
-      q: `'${process.env.ASSETS_PATH}' in parents and name='${image.assetName}'`,
-
-      fields: 'files(id)'
-    });
-
-    const driveId = res.data.files[0].id;
-
-    await drive.files.update({
-      fileId: driveId,
-
-      auth: auth,
-
-      requestBody: {
-        mimeType: updateImageDto.file.mimetype ?? image.mimeType
-      },
-
-      media: {
-        mimeType: updateImageDto.file?.mimetype ?? image.mimeType,
-
-        body: updateImageDto.file ? updateImageDto.file.buffer : null
+    const hasher = createHash('md5');
+    const bucket = await this.getBucket();
+    const file = bucket.file(image.assetName);
+    const updated = await file.save(updateImageDto.buffer, {
+      metadata: {
+        contentType: updateImageDto.mimetype
       }
     });
-    return await this.imageRepository.save({ ...image, ...updateImageDto });
+    return await this.imageRepository.save({
+      id: id,
+      hash: hasher.update(updateImageDto.buffer).digest('hex').toString(),
+      mimeType: updateImageDto.mimetype,
+      size: updateImageDto.size
+    });
   }
 
   async remove(id: string) {
     const image = await this.imageRepository.findOneBy({ id: id });
-
-    const auth = await this.authorize();
-
-    const drive = new drive_v3.Drive({
-      auth: auth
-    });
-
-    const res = await drive.files.list({
-      q: `'${process.env.ASSETS_PATH}' in parents and name='${image.assetName}'`,
-
-      fields: 'files(id)'
-    });
-
-    const driveId = res.data.files[0].id;
-
-    await drive.files.delete({
-      fileId: driveId,
-
-      auth: auth
-    });
+    const bucket = await this.getBucket();
+    const file = bucket.file(image.assetName);
+    const removed = await file.delete();
+    console.log(removed);
     return await this.imageRepository.remove(image);
   }
 }
